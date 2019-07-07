ffaa2e266531d386dc6905bc15efa5e5d1cc86c3
Konrad C <konrad1988@interia.pl>
2019-07-07 12:26:14 +0200

    port in app.js

--- app.js
+++ app.js
@@ -142,7 +142,8 @@ app.delete("/blogs/:id", (req, res)=>{
     });
     // redirect
 });*/
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000, function(){
+app.listen(PORT, function(){
     console.log("Server is running!");
 });
