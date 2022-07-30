b93e0a6c38b54d93d73a23915a532babaa44ffa7
Havlc <konrad1988@interia.pl>
2022-07-30 18:02:06 +0200

    add geoportal project and clean-up app.js

--- app.js
+++ app.js
@@ -25,12 +25,6 @@ app.get("/form", (req,res)=>{
 app.get("/landingpage", (req,res)=>{
     res.render("landingpage")
 })
-app.get("/documentation", (req,res)=>{
-    res.render("techdoc")
-})
-app.get("/tribute", (req,res)=>{
-    res.render("tribute")
-})
 
 app.get("/todo", (req,res)=>{
     res.render("todo")
@@ -48,26 +42,10 @@ app.get("/businesscard", (req, res)=>{
     res.render("BChome")
 })
 
-app.get("/infracrete", (req, res)=>{
-    res.render("infracrete")
-})
-
 app.get("/blog", (req, res)=>{
     res.render("blog")
 })
-/*
-app.get("/businesscard/whoami", (req, res)=>{
-    res.render("BCwhoami")
-})
-app.get("/businesscard/cv", (req, res)=>{
-    res.render("BCcv")
-})
-app.get("/businesscard/contact", (req, res)=>{
-    res.render("BCcontact")
-})
-app.get("/businesscard/offer", (req, res)=>{
-    res.render("BCoffer")
-})*/
+
 app.get("/caesar", (req, res)=>{
     res.render("Caesars_cipher")
 })
@@ -81,67 +59,6 @@ app.get("/telephone", (req, res)=>{
     res.render("Telephone_Number_Validator")
 })
 
-// INDEX ROUTE
-/*app.get("/blogs", (req,res)=>{
-    Blog.find({}, (err, blogs)=>{
-        if(err){
-            console.log("ERROR!");
-        } else{
-            res.render("index", {blogs: blogs});
-        }
-    });
-});
-
-// NEW ROUTE
-app.get("/blogs/new", (req,res)=>{
-    res.render("new");
-});
-
-// SHOW ROUTE
-app.get("/blogs/:id", (req, res)=>{
-        Blog.findById(req.params.id, (err, foundBlog)=>{
-            if (err){
-                res.redirect("/blogs");
-            } else{
-                res.render("show", {blog: foundBlog})
-            }
-        });
-    });
-
-// EDIT ROUTE
-app.get("/blogs/:id/edit", (req,res)=>{
-    Blog.findById(req.params.id, (err, foundBlog)=>{
-        if (err){
-            res.redirect("/blogs");
-        } else {
-            res.render("edit", {blog: foundBlog});
-        }
-    });
-});
-
-// UPDATE ROUTE
-app.put("/blogs/:id/", (req, res)=>{
-    req.body.blog.body = req.sanitize(req.body.blog.body)
-   Blog.findByIdAndUpdate(req.params.id, req.body.blog, (err, updatedBlog)=>{
-       if (err){
-           res.redirect("/blogs");
-       } else {
-           res.redirect("/blogs/"+req.params.id);
-       }
-   });
-});
-// DELETE ROUTE
-app.delete("/blogs/:id", (req, res)=>{
-    //destroy blog
-    Blog.findByIdAndRemove(req.params.id, (err)=>{
-        if(err){
-            res.redirect("/blogs");
-        } else {
-            res.redirect("/blogs");
-        }
-    });
-    // redirect
-});*/
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, function(){
